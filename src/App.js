0919c0c8c75fba1725a26093ee2eaa130830c1cf
Alex <manbow@mail.ru>
2023-07-02 21:54:10 +0300

    08.003 - Подготовка проекта, header и footer + Prettier

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import {Header} from './components/Header'
-import {Shop} from './components/Shop'
-import {Footer} from './components/Footer'
+import { Header } from './components/Header'
+import { Shop } from './components/Shop'
+import { Footer } from './components/Footer'
 
 function App() {
-    return (
-        <>
-            <Header/>
-            <Shop/>
-            <Footer/>
-        </>
-    )
+	return (
+		<>
+			<Header />
+			<Shop />
+			<Footer />
+		</>
+	)
 }
 
-export default App;
+export default App
