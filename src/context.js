9f310f98166520393e10099ade2bc397f61c7e20
Alex <manbow@mail.ru>
2023-07-05 21:46:37 +0300

    09.004 Решение - доработка reducer и методов в context

--- src/context.js
+++ src/context.js
@@ -15,10 +15,20 @@ export const ContextProvider = ({ children }) => {
 	value.closeAlert = () => {
 		dispatch({ type: 'CLOSE_ALERT' })
 	}
-
+	value.addToBasket = (item) => {
+		dispatch({ type: 'ADD_TO_BASKET', payload: item })
+	}
+	value.incQuantity = (itemId) => {
+		dispatch({ type: 'INCREMENT_QUANTITY', payload: { id: itemId } })
+	}
+	value.decQuantity = (itemId) => {
+		dispatch({ type: 'DECREMENT_QUANTITY', payload: { id: itemId } })
+	}
 	value.removeFromBasket = (itemId) => {
 		dispatch({ type: 'REMOVE_FROM_BASKET', payload: { id: itemId } })
 	}
-
+	value.handleBasketShow = () => {
+		dispatch({ type: 'TOGGLE_BASKET_SHOW' })
+	}
 	return <ShopContext.Provider value={value}>{children}</ShopContext.Provider>
 }
